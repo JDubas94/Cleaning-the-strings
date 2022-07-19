5bf347dcac046c297d78a373238a6f1590c5e8ce
Julia Dubas <uliadubas94@gmail.com>
2022-07-19 16:47:13 +0300

    Add project files.

---
+++ Cleaning the strings/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cleaning_the_strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = "This looks5 grea8t!" ;
+
+            string str = "";
+
+            for (int i = 0; i < x.Length; i++)
+            {
+                if (!char.IsDigit(x[i]))
+                {
+                    str += x[i];
+                }
+            }
+            Console.WriteLine(str);
+
+            Console.ReadLine();
+        }
+    }
+}
